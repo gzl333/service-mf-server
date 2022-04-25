3ca412bb08e53971baf92a84f65cf2e0be0a1746
mimas <zlguo@cnic.cn>
2022-04-25 18:53:33 +0800

    feat: 建立部分路由

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -14,13 +14,34 @@ const routes: RouteRecordRaw[] = [
           {
             path: 'list',
             component: () => import('pages/personal/ServerList.vue')
+          },
+          {
+            path: 'create',
+            component: () => import('pages/personal/ServerCreate.vue'),
+            props: true // 接收url中的参数
+          },
+          {
+            path: 'detail/:id', // serverId 动态路由匹配
+            component: () => import('pages/personal/ServerDetail.vue')
           }
         ]
       },
       {
         path: 'group',
         component: () => import('pages/group/GroupIndex.vue'),
-        children: []
+        children: [
+          {
+            path: 'list', // 组列表
+            component: () => import('pages/group/GroupList.vue')
+          },
+          {
+            path: 'serverlist', // 组资源
+            meta: {
+              requireLogin: true
+            },
+            component: () => import('pages/group/ServerList.vue')
+          }
+        ]
       },
       {
         path: 'vpn',
