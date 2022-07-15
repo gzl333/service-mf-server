ae3facf27ea196ab9747a564afaed727f2135268
zhjiang123 <1587162756@qq.com>
2022-07-15 11:26:14 +0800

    git test

--- src/i18n/zh/components.ts
+++ src/i18n/zh/components.ts
@@ -1,13 +1,45 @@
 // component translation
 export default {
   // { key: translation }
-  chart: {},
+  chart: {}, // void
   coupon: {
     CouponCard: {
       points: '点',
       service: '所属服务',
       expiration_time: '到期时间'
-    }
+    },
+    CouponRedeemInput: {
+      notification_input: '代金券输入有误，请检查输入',
+      notification_success:'成功兑换代金券',
+      redeem:'兑换',
+      redeem_group:'兑换到项目组',
+      redeem_personal: '兑换到个人账户'
+    },
+    CouponTable:{
+      Voucher_id:'代金券ID',
+      group:'所属组',
+      service_node:'服务节点',
+      redeem_date:'兑换日期',
+      expiration_date:'失效日期',
+      original_denomination:'原始面额',
+      available_balance:'可用余额',
+      status:'状态',
+      network_wait:'网络请求中，请稍候...',
+      novoucher:'暂无可用代金券',
+      noresult:'无搜索结果',
+      copy_clipboard:'复制到剪切板',
+      group_detail:'项目组详情',
+      available:'可用',
+      invalid:'失效',
+
+    },
+
+  RedeemCouponDialog:{
+
+
+  }
+
+
   },
   group: {},
   order: {},
