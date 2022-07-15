ac4920ad910fe1a5493d50306ca9b3476b615864
dykzzz <duyukuan@cnic.cn>
2022-07-15 14:20:48 +0800

    feat: 修正i18n格式

--- src/i18n/zh/components.ts
+++ src/i18n/zh/components.ts
@@ -10,53 +10,46 @@ export default {
     },
     CouponRedeemInput: {
       notification_input: '代金券输入有误，请检查输入',
-      notification_success:'成功兑换代金券',
-      redeem:'兑换',
-      redeem_group:'兑换到项目组',
+      notification_success: '成功兑换代金券',
+      redeem: '兑换',
+      redeem_group: '兑换到项目组',
       redeem_personal: '兑换到个人账户'
     },
-    CouponTable:{
-      vouchers_id:'代金券ID',
-      group:'所属组',
-      service_node:'服务节点',
-      redeem_date:'兑换日期',
-      expiration_date:'失效日期',
-      original_denomination:'原始面额',
-      available_balance:'可用余额',
-      status:'状态',
-      network_wait:'网络请求中，请稍候...',
-      novouchers:'暂无可用代金券',
-      noresults:'无搜索结果',
-      copy_clipboard:'复制到剪切板',
-      project_detail:'项目组详情',
-      available:'可用',
-      invalid:'失效',
+    CouponTable: {
+      vouchers_id: '代金券ID',
+      group: '所属组',
+      service_node: '服务节点',
+      redeem_date: '兑换日期',
+      expiration_date: '失效日期',
+      original_denomination: '原始面额',
+      available_balance: '可用余额',
+      status: '状态',
+      network_wait: '网络请求中，请稍候...',
+      novouchers: '暂无可用代金券',
+      noresults: '无搜索结果',
+      copy_clipboard: '复制到剪切板',
+      project_detail: '项目组详情',
+      available: '可用',
+      invalid: '失效'
 
     },
 
-  RedeemCouponDialog:{
-    redemption_error:'兑换码输入有误，请检查输入',
-    noavailable_project:'暂无可用项目组，请先创建或加入项目组',
-    redeem_vouchers:'兑换代金券',
-    redeem_to:'兑换到',
-    personal_account:'个人账户',
-    project_account:'项目组账户',
-    project_team:'项目组',
-    Content_slot_option :'当前选项的内容插槽',
-    redemption_code:'兑换码',
-    input_redemption_code:'请输入兑换码',
-    redeem:'兑换',
-    cancel:'取消',
-  }
+    RedeemCouponDialog: {
+      redemption_error: '兑换码输入有误，请检查输入',
+      noavailable_project: '暂无可用项目组，请先创建或加入项目组',
+      redeem_vouchers: '兑换代金券',
+      redeem_to: '兑换到',
+      personal_account: '个人账户',
+      project_account: '项目组账户',
+      project_team: '项目组',
+      Content_slot_option: '当前选项的内容插槽',
+      redemption_code: '兑换码',
+      input_redemption_code: '请输入兑换码',
+      redeem: '兑换',
+      cancel: '取消'
+    }
   },
-  group: {
-
-
-
-
-
-
-  },
+  group: {},
   order: {},
   personal: {},
   server: {},
