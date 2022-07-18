b8566bafa67927ca3c02f3505dabbf6e78262704
mimas <zlguo@cnic.cn>
2022-07-18 15:42:37 +0800

    feat: i18n-layouts

--- src/i18n/en/layouts.ts
+++ src/i18n/en/layouts.ts
@@ -1,4 +1,14 @@
 // layouts translation
 export default {
   // { key: translation }
+  server: 'Cloud Server',
+  personal: 'Personal',
+  group: 'Group',
+  order: 'Order',
+  provider: 'Provider',
+  management: 'Resource Management',
+  personal_resource: 'Personal Resource',
+  group_resource: 'Group Resource',
+  redeem: 'Redeem Coupon',
+  release_time: 'Release Time'
 }
--- src/i18n/zh/layouts.ts
+++ src/i18n/zh/layouts.ts
@@ -1,4 +1,14 @@
 // layouts translation
 export default {
   // { key: translation }
+  server: '云主机',
+  personal: '个人',
+  group: '项目组',
+  order: '订单',
+  provider: '提供者',
+  management: '资源管理',
+  personal_resource: '个人资源',
+  group_resource: '项目组资源',
+  redeem: '兑换代金券',
+  release_time: '发布时间'
 }
